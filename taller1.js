d6fe024761deef94fdc10fbb09d7702494c9cec7
Jaymed_DLC <98895014+JaymedDLC@users.noreply.github.com>
2024-08-16 15:11:21 -0500

    Update taller1.js
    Añadido punto 4, corregido punto 1 y nuevo formato

--- taller1.js
+++ taller1.js
@@ -1,8 +1,17 @@
 // Taller 1 - Backeend
 
+
+//Punto 1
+console.log("Punto 1:")
+
 function convertidorTemp (temp) {
-	return temp * 9 / 5 + 35;
+	return temp * 9 / 5 + 32;
 }
+console.log(convertidorTemp(-40)); // -40 es el punto donde ambas temperaturas se encuentran
+
+
+//Punto 2
+console.log("Punto 2:")
 
 function resolvedor(a, b, c, signo) {
 	if (signo)  {
@@ -11,18 +20,41 @@ function resolvedor(a, b, c, signo) {
   	return (-b - (b**2 - 4*a*c)**(1/2)) / (2*a);
   }
 }
+console.log(resolvedor(1,5,4,1)); // 1 para '+' y 0 para '-' 
+console.log(resolvedor(1,5,4,0));
+
+
+//Punto 3
+console.log("Punto 3:")
 
 function mejorParidad (x) {
   	return !(x%2); 
 }
+console.log(mejorParidad(4));
 
 
-//Punto 1
-//console.log(convertidorTemp(10));
+//Punto 4
+console.log("Punto 4:")
 
-//Punto 2
-//console.log(resolvedor(1,5,4,1));
-//console.log(resolvedor(1,5,4,0));
+function peorParidad(x) {
+    let alternador = true;
+    let par = 0;
+    let impar = 0;
 
-//Punto 3
-//console.log(mejorParidad(4));
+    for (let i = 1; i <= x; i++) {
+        if (alternador) {
+            impar += 1;
+            alternador = false;
+        } else {
+            par += 1;
+            alternador = true;
+        }
+    }
+
+    if (impar > par) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log(peorParidad(794334066));
