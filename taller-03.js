25c894478f99b4ffc354657f9ab73c888dddf148
Jaymed_DLC <98895014+JaymedDLC@users.noreply.github.com>
2024-09-06 11:21:55 -0500

    Update taller-03.js
    Punto 1

--- taller-03.js
+++ taller-03.js
@@ -1,6 +1,20 @@
 //Punto 1
-function desglosarString(palabra, opción) {
-  return 0
+function filterVocales(item) {
+    if (["a", "e", "i", "o", "u"].includes(item)) {
+        return true
+    }
+    return false
+}
+
+function desglosarString(palabra, opcion) {
+  const lista =  palabra.split("")
+  if (opcion == "vocales") {
+    return lista.filter(filterVocales).length
+  } 
+  if (opcion == "consonantes") {
+    return palabra.length - lista.filter(filterVocales).length
+  }
+  return Error
 }
 
 //Punto 2
