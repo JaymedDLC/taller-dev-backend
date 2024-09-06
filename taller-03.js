38fed581f5d690d276c49f4d141f457edfc79cab
Jaymed_DLC <98895014+JaymedDLC@users.noreply.github.com>
2024-09-06 11:21:29 -0500

    Create taller-03.js

---
+++ taller-03.js
@@ -0,0 +1,14 @@
+//Punto 1
+function desglosarString(palabra, opción) {
+  return 0
+}
+
+//Punto 2
+function twoSum(lista, total) {
+  return []
+}
+
+//Punto 3
+function conversionRomana(numRomano) {
+  return 0
+}
